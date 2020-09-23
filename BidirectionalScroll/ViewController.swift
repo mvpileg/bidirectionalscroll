2a7a9f5646d60527cefc8dc20147f6a76744b3a8
Matt Pileggi <mpileggi@linkedin.com>
2020-09-23 14:04:34 -0700

    Initial Commit

---
+++ BidirectionalScroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BidirectionalScroll
+//
+//  Created by Matthew Pileggi on 9/23/20.
+//  Copyright © 2020 Matthew Pileggi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
